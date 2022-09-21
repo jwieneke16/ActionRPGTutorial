6bc0b75eda74c67cbcbd00a5feb1e3868de78e92
jwieneke16 <joedog31616@gmail.com>
2022-09-20 19:28:05 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ActionRPGTutorial
+
+Developed with Unreal Engine 5
---
+++ Source/ActionRPGTutorial/ActionRPGTutorialGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRPGTutorialGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AActionRPGTutorialGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AActionRPGTutorialGameMode();
+};
+
+
+
